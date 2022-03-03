31325ce29b246db68259b85bf3fe331232452c03
Marc Bäckman <marc.backman@edu.lapinamk.fi>
2022-03-03 03:02:35 +0200

    Harjoitus 1 ja 2 kaikki tehtävät tehty.

--- Harjoitukset/MainPage.xaml.cs
+++ Harjoitukset/MainPage.xaml.cs
@@ -20,7 +20,7 @@ using Windows.UI.Xaml.Navigation;
 namespace Harjoitukset
 {
 	/// <summary>
-	/// An empty page that can be used on its own or navigated to within a Frame.
+	/// An empty page that can be used on its own or navigated to within a Frame
 	/// </summary>
 	public sealed partial class MainPage : Page
 	{
