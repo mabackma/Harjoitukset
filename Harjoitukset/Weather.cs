88272ee042f769a3658087c18f0bbfce617c9b11
Marc Bäckman <marc.backman@edu.lapinamk.fi>
2022-03-07 01:55:25 +0200

    H3 lisätehtävä 3 tehty.

---
+++ Harjoitukset/Weather.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harjoitukset
+{
+    public class Weather
+    {
+        public string Location { get; set; }
+        public double Snow { get; set; }
+        public double Rain { get; set; }
+        public double Wind { get; set; }
+
+        public Weather()
+        {
+            Location = "Ei nimeä";
+            Snow = 0.0;
+            Rain = 0.0;
+            Wind = 0.0;
+        }
+
+        public String Summary
+        {
+            get
+            {
+                return Location;
+            }
+        }
+
+    }
+}
---
+++ Harjoitukset/WeatherViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Data.Json;
+
+namespace Harjoitukset
+{
+	public class WeatherViewModel
+	{
+		// HUOM: tyyppinä on ObservableCollection, joka automaattisesti
+		// huolehtii Data Bindingin vaatimasta 
+		// PropertyChanged-ominaisuudesta!
+		private ObservableCollection<Weather> weathers = new ObservableCollection<Weather>();
+
+		// tehdään myös property ylläolevasta muuttujasta
+		public ObservableCollection<Weather> Weathers { get { return this.weathers; } }
+
+		public WeatherViewModel()
+		{
+			String JSON_URL = "https://edu.frostbit.fi/api/weather/";
+
+			// contents muuttujan sisältö voi tulla tiedostostakin,
+			// ks. tiedoston lukeminen, esimerkit Moodlessa
+			string contents;
+			using (var wc = new System.Net.WebClient())
+				contents = wc.DownloadString(JSON_URL);
+
+
+			// on hyvä idea testata etukäteen vaikka näin että tulihan raaka-JSON perille asti
+			//Debug.WriteLine(contents);
+
+			// muutetaan taulukoksi
+			JsonArray weatherItems = JsonArray.Parse(contents);
+
+			// lisätään jokainen säätiedotus silmukassa observablecollectioniin
+			foreach (JsonValue objvalue in weatherItems)
+			{
+				JsonObject obj = objvalue.GetObject() as JsonObject;
+
+				this.weathers.Add(new Weather()
+				{
+					Location = obj.GetNamedString("location"),
+					Snow = obj.GetNamedNumber("snow"),
+					Rain = obj.GetNamedNumber("rain"),
+					Wind = obj.GetNamedNumber("wind")
+				});
+			}
+
+		}
+	}
+}
